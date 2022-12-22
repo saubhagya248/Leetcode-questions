2cf5c1dc2fb7f7de55b9fb2a93b20861cdfc0d65
saubhagya248 <58850321+saubhagya248@users.noreply.github.com>
2022-12-22 21:43:27 +0530

    Added solution - LeetHub

---
+++ Triangular Number - GFG/triangular-number.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N=Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isTriangular(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isTriangular(int n){
+        int start = 1;
+		int end = n;
+
+		while(start<=end){
+			int mid = (start+end)/2;
+			int num = (mid*(mid+1)/2);
+			if(num==n) return 1;
+			else if(num>n) end = mid-1;
+			else start = mid+1;
+		}
+
+		return 0;
+    }
+}
